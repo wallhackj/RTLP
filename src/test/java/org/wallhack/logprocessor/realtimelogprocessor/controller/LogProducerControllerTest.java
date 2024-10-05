eb4bb3707b794ec1dbe10d7b17e652180cb46979
Omnissiah <victorhaideu@gmail.com>
2024-10-05 12:29:52 +0300

    test 5.4

--- src/test/java/org/wallhack/logprocessor/realtimelogprocessor/controller/LogProducerControllerTest.java
+++ src/test/java/org/wallhack/logprocessor/realtimelogprocessor/controller/LogProducerControllerTest.java
@@ -5,6 +5,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -19,6 +20,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 @WebFluxTest(LogProducerController.class)
+@SpringBootTest
 class LogProducerControllerTest {
     @Autowired
     private WebTestClient webTestClient;
