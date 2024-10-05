739ccf7a51179904907280c1cac520bb717cf0d9
Omnissiah <victorhaideu@gmail.com>
2024-10-05 19:10:12 +0300

    test 2 fix 1

--- src/test/java/org/wallhack/logprocessor/realtimelogprocessor/controller/LogProducerControllerTest.java
+++ src/test/java/org/wallhack/logprocessor/realtimelogprocessor/controller/LogProducerControllerTest.java
@@ -1,53 +1,57 @@
-package org.wallhack.logprocessor.realtimelogprocessor.controller;
+    package org.wallhack.logprocessor.realtimelogprocessor.controller;
 
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.context.aot.DisabledInAotMode;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.wallhack.logprocessor.realtimelogprocessor.service.dto.LogDTO;
+    import org.junit.jupiter.api.Test;
+    import org.mockito.ArgumentCaptor;
+    import org.mockito.InjectMocks;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+    import org.springframework.boot.test.mock.mockito.MockBean;
+    import org.springframework.http.MediaType;
+    import org.springframework.kafka.core.KafkaTemplate;
+    import org.springframework.test.context.aot.DisabledInAotMode;
+    import org.springframework.test.web.reactive.server.WebTestClient;
+    import org.wallhack.logprocessor.realtimelogprocessor.service.dto.LogDTO;
 
-import java.time.Instant;
-import java.util.Date;
+    import java.time.Instant;
+    import java.util.Date;
+    import java.util.concurrent.CompletableFuture;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
+    import static org.assertj.core.api.Assertions.assertThat;
+    import static org.mockito.ArgumentMatchers.eq;
+    import static org.mockito.Mockito.verify;
+    import static org.mockito.Mockito.when;
 
-@DisabledInAotMode
-@WebFluxTest(LogProducerController.class)
-class LogProducerControllerTest {
-    @Autowired
-    private WebTestClient webTestClient;
+    @DisabledInAotMode
+    @WebFluxTest(LogProducerController.class)
+    class LogProducerControllerTest {
+        @Autowired
+        private WebTestClient webTestClient;
 
-    @MockBean
-    private KafkaTemplate<String, LogDTO> kafkaTemplate;
+        @MockBean
+        private KafkaTemplate<String, LogDTO> kafkaTemplate;
 
-    @InjectMocks
-    private LogProducerController controller;
+        @InjectMocks
+        private LogProducerController controller;
 
-    private final static String LOG_ENDPOINT = "/log";
+        private final static String LOG_ENDPOINT = "/log";
 
-    @Test
-    public void testSendLog() {
-        var logDTO = new LogDTO("This is a test log message", "INFO", Date.from(Instant.now()));
+        @Test
+        public void testSendLog() {
+            var logDTO = new LogDTO("This is a test log message", "INFO", Date.from(Instant.now()));
 
-        webTestClient.post()
-                .uri(LOG_ENDPOINT)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(logDTO)
-                .exchange()
-                .expectStatus().isOk();
+            when(kafkaTemplate.send(eq("realtimelog"), eq(logDTO))).thenReturn(CompletableFuture.completedFuture(null));
 
-        ArgumentCaptor<LogDTO> captor = ArgumentCaptor.forClass(LogDTO.class);
-        System.out.println(controller);
+            webTestClient.post()
+                    .uri(LOG_ENDPOINT)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(logDTO)
+                    .exchange()
+                    .expectStatus().isOk();
 
-        verify(kafkaTemplate).send(eq("test-log-topic"), captor.capture());
-        assertThat(captor.getValue()).isEqualTo(logDTO);
+            ArgumentCaptor<LogDTO> captor = ArgumentCaptor.forClass(LogDTO.class);
+            System.out.println(controller);
+
+            verify(kafkaTemplate).send(eq("realtimelog"), captor.capture());
+            assertThat(captor.getValue()).isEqualTo(logDTO);
+        }
     }
-}
