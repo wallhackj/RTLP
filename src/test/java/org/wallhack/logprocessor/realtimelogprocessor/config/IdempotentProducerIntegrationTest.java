eb9bd0bd4b919d0109438ce2c327394b19f79859
Omnissiah <victorhaideu@gmail.com>
2024-10-05 18:29:49 +0300

    test

---
+++ src/test/java/org/wallhack/logprocessor/realtimelogprocessor/config/IdempotentProducerIntegrationTest.java
@@ -0,0 +1,41 @@
+package org.wallhack.logprocessor.realtimelogprocessor.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.wallhack.logprocessor.realtimelogprocessor.service.dto.LogDTO;
+
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class IdempotentProducerIntegrationTest {
+    @Autowired
+    private KafkaTemplate<String, LogDTO> kafkaTemplate;
+
+    @MockBean
+    private KafkaAdmin admin; // to make test faster ,because i don't work with topic in this test
+
+    @Test
+    public void testProducerConfig_whenIdempotentEnabled() {
+        System.out.println(admin);
+        ProducerFactory<String, LogDTO> factory = kafkaTemplate.getProducerFactory();
+
+        Map<String, Object> config = factory.getConfigurationProperties();
+
+        assertThat(config.get(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG)).isEqualTo("true");
+        assertThat(config.get(ProducerConfig.ACKS_CONFIG)).isEqualTo("all");
+
+        if (config.containsKey(ProducerConfig.RETRIES_CONFIG)) {
+            assertThat(Integer.parseInt(
+                    config.get(ProducerConfig.RETRIES_CONFIG).toString()) > 0)
+                    .isEqualTo("true");
+        }
+    }
+}
